b37e245aa3f9847417eab3654c5a8172bd011bb2
Kaibito <gpuzzletime@icloud.com>
2017-03-07 16:24:51 +0300

    Button and Input changes

---
+++ static/app/components/Input/input.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+window.addEventListener('CreateComponents', () => {
+
+	window.Component.Input = class extends window.Framework.ComponentStub.Input {
+		constructor() {
+			super();
+		}
+	};
+
+});
