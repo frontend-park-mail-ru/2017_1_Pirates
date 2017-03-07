46698ac9121f164decf50c245f2bc71a33b5ddbf
vileven <vileven11@icloud.com>
2017-03-07 19:01:43 +0300

    Created Loading component

---
+++ static/app/components/Loading/loading.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+window.addEventListener('CreateComponents', () => {
+	window.Component.Loading = class extends window.Framework.ComponentStub.Loading {
+		constructor() {
+			super();
+
+			window.setTimeout(() => {
+				this.setup();
+			}, 100);
+		}
+
+		setup() {
+			const load = this.view.querySelector('div');
+
+			const radius = 14;
+			const circlesNumber = 8;
+			const wrap = 64 - 4;
+
+			for (let i = 0; i < circlesNumber; i++) {
+				const circle = document.createElement('span');
+				const f = 2 / circlesNumber * i * Math.PI;
+
+				circle.setAttribute('class', 'circle flicker');
+				circle.style.left = (Math.cos(f) * radius + wrap / 2) + 'px';
+				circle.style.top = (Math.sin(f) * radius + wrap / 2) + 'px';
+
+				load.appendChild(circle);
+			}
+		}
+	};
+});
