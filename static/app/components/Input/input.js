19e55373000588ce300c2045325a8fc023a61d47
Kaibito <gpuzzletime@icloud.com>
2017-03-07 19:03:24 +0300

    Working on Input.onTextChange

--- static/app/components/Input/input.js
+++ static/app/components/Input/input.js
@@ -6,6 +6,16 @@ window.addEventListener('CreateComponents', () => {
 	window.Component.Input = class extends window.Framework.ComponentStub.Input {
 		constructor() {
 			super();
+			this.chain = new window.Framework.ValidatorChain()
+		}
+
+		get valid() {
+			return false;
+		}
+
+		onTextChange(value) {
+			console.log(value);
+			return value;
 		}
 	};
 
