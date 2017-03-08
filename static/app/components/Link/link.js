17fd4bdb88f80b93573011c9f872e489e1057c49
Kaibito <gpuzzletime@icloud.com>
2017-03-09 02:23:19 +0300

    Added Vileven's Link component

---
+++ static/app/components/Link/link.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+window.addEventListener('CreateComponents', () => {
+
+	window.Component.Link = class extends window.Framework.ComponentStub.Link {
+		constructor() {
+			super();
+		}
+
+	}
+
+});
