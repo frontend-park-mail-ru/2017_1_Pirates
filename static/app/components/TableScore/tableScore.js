003e4ad02a8191c2a100987b56e6d00a99f372cd
vileven <vileven11@icloud.com>
2017-03-10 03:43:53 +0300

    Created TableScore component

--- static/app/components/TableScore/tableScore.js
+++ static/app/components/TableScore/tableScore.js
@@ -10,6 +10,34 @@ window.addEventListener('CreateComponents', () => {
 		}
 
 
+		onDataChange(data) {
+
+			const fakeData = {'lol':1488, 'kek':1941, 'cheburek':11};
+
+			const table = this.view.querySelector('.table');
+
+			Object.keys(fakeData).forEach(user => {
+				let row = document.createElement('view-row');
+				row.setAttribute('width', '4');
+				row.appendChild(document.createElement('view-column'));
+
+				let col = document.createElement('view-column');
+				col.setAttribute('width', '1');
+				col.textContent = user;
+				row.appendChild(col);
+
+				col = document.createElement('view-column');
+				col.setAttribute('width', '1');
+				col.textContent = fakeData[user];
+				row.appendChild(col);
+
+				row.appendChild(document.createElement('view-column'));
+
+				table.appendChild(row);
+			});
+
+			return data;
+		}
 	}
 
 });
